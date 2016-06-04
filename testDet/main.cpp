9c6e4d91127b69ab746d51fd0341822ab7a04b49
vc <vinceplusplus@gmail.com>
2016-06-04 09:15:59 +0800

    Initial Commit

---
+++ testDet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testDet
+//
+//  Created by Vincent Cheung on 6/4/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
